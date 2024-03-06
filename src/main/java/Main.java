607299e5dffafd0fd743c3e8b7ee9cf10342e53d
Danny Cao <dannycao1997@yahoo.com>
2024-03-06 09:54:59 -0500

    starting

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,6 @@
+import java.util.Random;
+import java.util.Scanner;
+
 /**
  * Created by iyasuwatts on 10/17/17.
  */
@@ -6,4 +9,20 @@ public class Main {
     public static void main(String[] args){
         
     }
+
+    public static int getRandomNumber() {
+        Random random = new Random();
+        int randomNumber = random.nextInt(5);
+        return randomNumber;
+    }
+
+    public static void startGame() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a number: ");
+        
+
+    }
 }
+
+
+
