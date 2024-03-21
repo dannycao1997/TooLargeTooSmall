976eadfac6ad7ae10a77448f69389a16cb30ce67
Danny Cao <dannycao1997@yahoo.com>
2024-03-21 14:31:38 -0400

    made some changes to the code to add game to casino

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -21,17 +21,34 @@ public class Main {
     public static void startGame() {
         Scanner scanner = new Scanner(System.in);
         int randomGuess = getRandomNumber();
-
+        System.out.println("\uD835\uDD4E\uD835\uDD56\uD835\uDD5D\uD835\uDD54\uD835\uDD60\uD835\uDD5E\uD835\uDD56 \uD835\uDD65\uD835\uDD60 ℕ\uD835\uDD66\uD835\uDD5E\uD835\uDD53\uD835\uDD56\uD835\uDD63 \uD835\uDD3E\uD835\uDD66\uD835\uDD56\uD835\uDD64\uD835\uDD64❕");
+        System.out.println("⠀⠀⠀⣀⣤⣶⡶⠿⢿⣿⣶⣶⣤⣀⠀⠀⠀\n" +
+                "⠀⢀⣾⣿⠋⠁⠀⠀⠀⠀⠙⢿⣿⣿⣷⣄⠀\n" +
+                "⠀⣿⣿⣿⡀⠀⠀⠀⠀⠀⠀⠈⣿⣿⣿⣿⡄\n" +
+                "⠘⣿⣿⣿⣷⠀⠀⠀⠀⠀⠀⠀⢸⣿⣿⣿⡇\n" +
+                "⠀⠹⢿⣿⠟⠀⠀⠀⠀⠀⠀⠀⣼⣿⣿⣿⠇\n" +
+                "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⣿⣿⣿⠟⠀\n" +
+                "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⣿⣿⡿⠋⠀⠀\n" +
+                "⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⣿⣿⠟⠁⠀⠀⠀\n" +
+                "⠀⠀⠀⠀⠀⠀⠀⠀⢠⣿⡿⠁⠀⠀⠀⠀⠀\n" +
+                "⠀⠀⠀⠀⠀⠀⠀⠀⣾⡏⠀⠀⠀⠀⠀⠀⠀\n" +
+                "⠀⠀⠀⠀⠀⠀⠀⢰⣿⠀⠀⠀⠀⠀⠀⠀⠀\n" +
+                "⠀⠀⠀⠀⠀⠀⠀⠘⠋⠀⠀⠀⠀⠀⠀⠀⠀\n" +
+                "⠀⠀⠀⠀⠀⠀⠀⣀⣀⡀⠀⠀⠀⠀⠀⠀⠀\n" +
+                "⠀⠀⠀⠀⠀⠀⣾⣿⣿⣿⣦⠀⠀⠀⠀⠀⠀\n" +
+                "⠀⠀⠀⠀⠀⠀⢿⣿⣿⣿⠟⠀⠀⠀⠀⠀⠀\n" +
+                "⠀⠀⠀⠀⠀⠀⠀⠉⠉⠁⠀⠀⠀⠀⠀⠀⠀");
+        System.out.println("Ok, I'm thinking of a number between 0 and 10");
         while (true) {
-            System.out.println("Guess and enter a the random number from 0 - 10 : ");
+            System.out.println("Enter your guess :");
             int input = scanner.nextInt();
 
             if (input > randomGuess) {
-                System.out.println("Your input is greater than the number to guess");
+                System.out.println("Your guess is too high.");
             } else if (input < randomGuess) {
-                System.out.println("Your input is less than the number to guess");
+                System.out.println("Your guess is too low.");
             } else {
-                System.out.println("You guesses the correct number");
+                System.out.println("Congratulations! You guessed the correct number.");
                 break;
                 }
             }
